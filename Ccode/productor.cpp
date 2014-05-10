1ba51fce57d6bde9c34ff8d29cb58bfd8a5c912a
b00902130 <b00902130@ntu.edu.tw>
2014-05-10 12:47:57 +0800

    fix some bug.

--- Ccode/productor.cpp
+++ Ccode/productor.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void printTryTalkCollection(string id, string topic, int status, string speaker, string category, string place, string description, int vote, string imageURL);
+void printTryTalkCollection(string id, string topic, string status, string speaker, string category, string place, string description, string vote, string imageURL);
 void align(int numberOfRecursive);
 FILE *fp;
 
@@ -16,58 +16,49 @@ int main(){
 	fp = fopen("collection.json", "a+");
 	string id;
 	string topic;
-	int status;
+	string status;
 	string speaker;
 	string category;
 	string place;
 	string description;
-	int vote;
+	string vote;
 	string imageURL;
 	int over = 10;
 
 	while(1){
-
 		cout << "id: ";
-		stream.getline(&id[0],32);
-
+		getline(cin,id);
 		cout << "topic: ";
-		getline(&topic[0],32);
-
+		getline(cin,topic);
 		cout << "status: ";
-		scanf("%d",&status);
-
+		getline(cin,status);
 		cout << "speaker: ";
-		getline(&speaker[0],32);
-
+		getline(cin,speaker);
 		cout << "category: ";
-		getline(&category[0],32);
-
+		getline(cin,category);
 		cout << "place: ";
-		getline(&place[0],32);
-
+		getline(cin,place);
 		cout << "description: ";
-		getline(&description[0],32);
-
+		getline(cin,description);
 		cout << "vote: ";
-		scanf("%d",&vote);
-
+		getline(cin,vote);
 		cout << "imageURL: ";
-		getline(&imageURL[0],128);
+		getline(cin,imageURL);
 
 		cout << "keep input? : ";
 		scanf("%d",&over);
-
+		
+		
+		printTryTalkCollection(id,topic,status,speaker,category,place,description,vote,imageURL);
 		if(over == 0)
 			break;
-		printTryTalkCollection(id,topic,status,speaker,category,place,description,vote,imageURL);
-
 	}
 
 	fclose(fp);
 	return 0;
 }
 
-void printTryTalkCollection(string id, string topic, int status, string speaker, string category, string place, string description, int vote, string imageURL){
+void printTryTalkCollection(string id, string topic, string status, string speaker, string category, string place, string description, string vote, string imageURL){
 
 
 	fprintf(fp, "{\n");
@@ -76,7 +67,7 @@ void printTryTalkCollection(string id, string topic, int status, string speaker,
 	align(1);
 	fprintf(fp, "\"topic\" : \"%s\",\n", topic.c_str());
 	align(1);
-	fprintf(fp, "\"status\" : \"%d\",\n", status);
+	fprintf(fp, "\"status\" : \"%s\",\n", status.c_str());
 	align(1);
 	fprintf(fp, "\"speaker\" : \"%s\",\n", speaker.c_str());	
 	align(1);
@@ -86,7 +77,7 @@ void printTryTalkCollection(string id, string topic, int status, string speaker,
 	align(1);
 	fprintf(fp, "\"description\" : \"%s\",\n", description.c_str());		
 	align(1);
-	fprintf(fp, "\"vote\" : \"%d\",\n", vote);	
+	fprintf(fp, "\"vote\" : \"%s\",\n", vote.c_str());	
 	align(1);
 	fprintf(fp, "\"imageURL\" : \"%s\"\n", imageURL.c_str());	
 
