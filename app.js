310bfe4f992f6adf552e87c06997d89b75421c41
tony056 <tony61507@gmail.com>
2014-05-10 10:20:59 +0800

    basic structure

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
 var express = require('express');
+var routes = require('./routes');
+var index = require('./routes/index');
+var http = require('http');
+var path = require('path');
+var portNum = process.env.PORT || 3000;
+var mongo = require('mongoskin');
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/robotserver'; 
+var db = mongo.db(mongoUri, {native_parser:true});
+
 var app = express();
+
+// all environments
+
+app.set('port', portNum);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', index.load);
+app.post('/create', index.create);
+app.get('/search', index.search);
+//add the url of your function
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,20 @@
-var express = require('express');
+exports.load = function(req, res){
+	// home page to 
+};
+
+exports.create = function(db){
+	//create collection 
+	return function(req, res){
+
+	}
+};
+
+exports.search = function(db){
+	return function(req, res){
+
+	}
+};
+
+exports.userAuth = function(req, res){
+
+};
